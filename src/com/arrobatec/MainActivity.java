6f114c0c490740567e55f4736a030c0895cb5a42
Bartdd3 <leandroandrele@gmail.com>
2014-09-29 15:02:55 -0300

    ate sem erro

--- src/com/arrobatec/MainActivity.java
+++ src/com/arrobatec/MainActivity.java
@@ -1,17 +1,39 @@
 package com.arrobatec;
 
 import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
+	
+	Button bttela1, bttela2,bttela3,bttela4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        
+        bttela1 = (Button) findViewById(R.id.bttela1);
+        bttela2 = (Button) findViewById(R.id.bttela2);
+        bttela3 = (Button) findViewById(R.id.bttela3);
+        bttela4 = (Button) findViewById(R.id.bttela4);
+        
+        bttela2.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent TrocaTela = new Intent(MainActivity.this,Main_tela1.class);
+				MainActivity.this.startActivity(TrocaTela);
+				MainActivity.this.finish();
+			}
+		});
     }
 
 
